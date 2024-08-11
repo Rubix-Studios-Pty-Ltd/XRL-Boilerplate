4e177ff635b84c25917017b13d45b4b8d382ec5f
Vincent Vu <vincent@rubixstudios.com.au>
2024-08-11 17:57:50 +1000

    Initial commit

---
+++ nextjs-supabase-stripe/components/ui/AuthForms/UpdatePassword.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import { updatePassword } from '@/utils/auth-helpers/server';
+import { handleRequest } from '@/utils/auth-helpers/client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+interface UpdatePasswordProps {
+  redirectMethod: string;
+}
+
+export default function UpdatePassword({
+  redirectMethod
+}: UpdatePasswordProps) {
+  const router = useRouter(); // Always call the hook
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsSubmitting(true); // Disable the button while the request is being handled
+
+    // Conditionally use the router based on redirectMethod
+    const nextRouter = redirectMethod === 'client' ? router : null;
+    await handleRequest(e, updatePassword, nextRouter);
+
+    setIsSubmitting(false);
+  };
+
+  return (
+    <div className="my-8">
+      <form
+        noValidate={true}
+        className="mb-4"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <label htmlFor="password">New Password</label>
+            <input
+              id="password"
+              placeholder="Password"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              className="w-full p-3 rounded-md bg-zinc-800"
+            />
+            <label htmlFor="passwordConfirm">Confirm New Password</label>
+            <input
+              id="passwordConfirm"
+              placeholder="Password"
+              type="password"
+              name="passwordConfirm"
+              autoComplete="current-password"
+              className="w-full p-3 rounded-md bg-zinc-800"
+            />
+          </div>
+          <Button
+            variant="slim"
+            type="submit"
+            className="mt-1"
+            loading={isSubmitting}
+          >
+            Update Password
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
